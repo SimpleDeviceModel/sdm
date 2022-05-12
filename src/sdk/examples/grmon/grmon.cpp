bda6e3bc6406b2a25d396edd86ea7825b6f2ddab
catharanthus <catharanthus@users.noreply.github.com>
2022-05-12 20:58:23 +0300

    grmon: cleanup

--- src/sdk/examples/grmon/grmon.cpp
+++ src/sdk/examples/grmon/grmon.cpp
@@ -50,9 +50,9 @@ SDMAbstractPlugin *SDMAbstractPlugin::instance() {
  */
 
 GrmonPlugin::GrmonPlugin() {
-	addConstProperty("Name","GRMON protocol plugin");
+	addConstProperty("Name","GRMON protocol");
 	addConstProperty("Vendor","Alex I. Kuznetsov");
-	addListItem("Devices","GRMON AHBUART");
+	addListItem("Devices","AHB UART");
 }
 
 SDMAbstractDevice *GrmonPlugin::openDevice(int id) {
@@ -65,7 +65,7 @@ SDMAbstractDevice *GrmonPlugin::openDevice(int id) {
  */
 
 GrmonDevice::GrmonDevice() {
-	addConstProperty("Name","GRMON AHBUART");
+	addConstProperty("Name","AHB UART");
 // Make sdmconsole open channels and sources automatically
 	addConstProperty("AutoOpenChannels","open");
 	addConstProperty("AutoOpenSources","open");
@@ -132,37 +132,13 @@ int GrmonChannel::close() {
 }
 
 int GrmonChannel::writeReg(sdm_addr_t addr,sdm_reg_t data) {
-// Send "Write register" command packet: 11 (LENGTH-1)[5:0] ADDR[31:0] DATA[31:0]
-	std::vector<Byte> packet(9);
-	packet[0]=0xC0;
-	packet[1]=(addr>>24)&0xFF;
-	packet[2]=(addr>>16)&0xFF;
-	packet[3]=(addr>>8)&0xFF;
-	packet[4]=(addr)&0xFF;
-	packet[5]=(data>>24)&0xFF;
-	packet[6]=(data>>16)&0xFF;
-	packet[7]=(data>>8)&0xFF;
-	packet[8]=(data)&0xFF;
-	sendBytes(packet);
+	writeMem(addr,&data,1);
 	return 0;
 }
 
 sdm_reg_t GrmonChannel::readReg(sdm_addr_t addr,int *status) {
-// Send "Read register" command packet: 10 (LENGTH-1)[5:0] ADDR[31:0]
-	std::vector<Byte> packet(5);
-	packet[0]=0x80;
-	packet[1]=(addr>>24)&0xFF;
-	packet[2]=(addr>>16)&0xFF;
-	packet[3]=(addr>>8)&0xFF;
-	packet[4]=(addr)&0xFF;
-	sendBytes(packet);
-// Receive data
-	auto const &r=recvBytes(4);
-	sdm_reg_t data=0;
-	for(int i=0;i<4;i++) {
-		data=data<<8;
-		data|=r[i];
-	}
+	sdm_reg_t data;
+	readMem(addr,&data,1);
 	return data;
 }
 
--- src/sdk/examples/grmon/readme.txt
+++ src/sdk/examples/grmon/readme.txt
@@ -1,5 +1,11 @@
-This plugin implements the Gaisler AHBUART protocol.
+This plugin implements the Gaisler AHB UART protocol.
 
-See https://www.gaisler.com/products/grlib/grip.pdf
+Write command: 11 LEN[5:0] ADDR[31:0] DATA[31:0] ...
+
+Read command: 10 LEN[5:0] ADDR[31:0]
+
+LEN is number of words written (read) minus 1.
+
+See https://www.gaisler.com/products/grlib/grip.pdf for details.
 
 Gaisler is a trademark of Cobham Gaisler AB.
